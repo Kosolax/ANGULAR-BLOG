98bfaefe84bf5636aed0d1ba0a8617d2dba76785
Kosolax <ma.mininger@gmail.com>
2023-03-12 20:25:28 +0100

    Ajout de la liste (instable)

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: 'authentication', loadChildren: () => import('./feature/authentication/authentication.module').then(m => m.AuthenticationModule) },
   { path: 'admin/tags', loadChildren: () => import('./feature/admin/tag/tag.module').then(m => m.TagModule) },
   { path: 'admin/articles', loadChildren: () => import('./feature/admin/article/article.module').then(m => m.ArticleModule) },
+  { path: 'articles', loadChildren: () => import('./feature/article/article.module').then(m => m.ArticleModule) },
   { path: 'forbidden', component: ForbiddenComponent },
   { path: 'notfound', component: NotFoundComponent },
 ];
